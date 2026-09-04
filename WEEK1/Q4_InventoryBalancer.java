6f74f03c740e1b7c9a425db393791c57e677fb3f
tanishmudgal150807 <tanishmudgal845@gmail.com>
2026-09-04 22:16:08 +0530

    Add Week 1 assignments

---
+++ WEEK1/Q4_InventoryBalancer.java
@@ -0,0 +1,70 @@
+public class Q4_InventoryBalancer {
+
+    static void analyzeInventory(int[] sectionA, int[] sectionB) {
+
+        int totalA = 0;
+        int totalB = 0;
+
+        // Calculate total of Section A
+        for (int i = 0; i < sectionA.length; i++) {
+            totalA += sectionA[i];
+        }
+
+        // Calculate total of Section B
+        for (int i = 0; i < sectionB.length; i++) {
+            totalB += sectionB[i];
+        }
+
+        // Find highest quantity
+        int highest = sectionA[0];
+        String highestSection = "Section A";
+        int highestIndex = 0;
+
+        // Check Section A
+        for (int i = 0; i < sectionA.length; i++) {
+
+            if (sectionA[i] > highest) {
+                highest = sectionA[i];
+                highestSection = "Section A";
+                highestIndex = i;
+            }
+        }
+
+        // Check Section B
+        for (int i = 0; i < sectionB.length; i++) {
+
+            if (sectionB[i] > highest) {
+                highest = sectionB[i];
+                highestSection = "Section B";
+                highestIndex = i;
+            }
+        }
+
+        String status;
+
+        if (totalA == totalB) {
+            status = "Balanced";
+        } 
+        else {
+            status = "Not Balanced";
+        }
+
+        System.out.println("Section A Total: " + totalA);
+        System.out.println("Section B Total: " + totalB);
+        System.out.println("Status: " + status);
+
+        System.out.println(
+            "Highest Quantity: " + highest +
+            " (" + highestSection +
+            ", Item " + (highestIndex + 1) + ")"
+        );
+    }
+
+    public static void main(String[] args) {
+
+        int[] sectionA = {20, 15, 30};
+        int[] sectionB = {25, 10, 30};
+
+        analyzeInventory(sectionA, sectionB);
+    }
+}
